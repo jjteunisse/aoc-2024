2b2846c43765250b5e06142a620fa13e46fc99a1
Margot Teunisse <margotteunisse@hotmail.com>
2024-12-01 09:26:23 +0100

    Solved day 1

--- margot/day1.py
+++ margot/day1.py
@@ -2,12 +2,18 @@ import numpy as np
 import sys
 
 def main():
+    #Read data
     path = "inputs/day1/"
     name = "input"
     
     data = np.array([line.split() for line in open(path+name+".txt")], dtype=int)
 
+    #Part 1
     print("Total distance:", np.sum(np.absolute(np.diff(np.sort(data, axis=0), axis=1)), axis=0)[0])
+    
+    #Part 2
+    counts = {i:count for i, count in zip(*np.unique(data[:, 1], return_counts = True))}
+    print("Similarity score:", sum([i*counts[i] if i in counts else 0 for i in data[:, 0]]))
     
     return
     
