744d84c4836c0993324a57aec13de5be49e0e70f
Margot Teunisse <Teunisse@Physics.leidenuniv.nl>
2024-12-04 10:18:13 +0100

    Solved day 4, part 1

--- margot/day4.py
+++ margot/day4.py
@@ -1,23 +1,29 @@
 import sys
 import numpy as np
 import re
+import time
 
 def main():
     path = "inputs/day4/"
-    name = "test"
+    name = "input"
     
+    start = time.time()
     data = np.array([tuple(line.strip()) for line in open(path+name+".txt")])
-    print(data)
     
-    iterator = [row for row in data] + [row for row in data[:, ::-1]] + [col for col in data.T] + [col for col in data[::-1].T]
+    iterator = np.array([row for row in data] + [col for col in data.T] + [np.diagonal(data, i) for i in range(-data.shape[0], data.shape[1])] + [np.diagonal(data[:,::-1], i) for i in range(-data.shape[0], data.shape[1])], dtype=object)
     
     count = 0
     for row in iterator:
         line = "".join(row)
         for match in re.finditer("XMAS", line):
             count += 1
+        line_reversed = "".join(row[::-1])
+        for match in re.finditer("XMAS", line_reversed):
+            count += 1
         
+    end = time.time()
     print("Number of XMAS matches:", count)
+    print("Runtime", end-start)
     
     return
 
