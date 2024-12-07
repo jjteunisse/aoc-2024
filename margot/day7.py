5ce52559547d663f1b0bcdcb36ff42e031865ca6
Margot Teunisse <margotteunisse@hotmail.com>
2024-12-07 10:37:26 +0100

    Day 7:separated tasks & applied multithreading. Resulting increase in efficiency is 7s -> 2s runtime for task 2 w/ 8 cores.

--- margot/day7.py
+++ margot/day7.py
@@ -1,39 +1,57 @@
 import sys
 import itertools
 from typing import Tuple
+import time
+import multiprocessing as mp
+
+def read(line:str) -> Tuple[int, Tuple[int]]:
+    groups = line.strip().split(":")
+    test_val = int(groups[0])
+    calibration_vals = tuple(int(val) for val in groups[1].split())
+    return test_val, calibration_vals
+
+def calibration_result(line:str) -> int:
+    test_val, calibration_vals = read(line)
+    
+    queue = {calibration_vals[0]}
+    for val2 in calibration_vals[1:]:
+        queue_new = {val1+val2 for val1 in queue}
+        queue_new.update({val1*val2 for val1 in queue})
+        queue = queue_new
+    return test_val*any([val == test_val for val in queue])
+
+def calibration_result_with_concatenation(line:str) -> int:
+    test_val, calibration_vals = read(line)
+    
+    queue = {calibration_vals[0]}
+    for val2 in calibration_vals[1:]:
+        queue_new = {val1+val2 for val1 in queue}
+        queue_new.update({val1*val2 for val1 in queue})
+        queue_new.update({int(str(val1)+str(val2)) for val1 in queue})
+        queue = queue_new
+    return test_val*any([val == test_val for val in queue])
 
 def main():
     path = "inputs/day7/"
     name = "input"
     
-    count = 0
-    count_with_concatenation = 0
-    for line in open(path+name+".txt"):
-        groups = line.strip().split(":")
-        test_val = int(groups[0])
-        calibration_vals = tuple(int(val) for val in groups[1].split())
-        
-        #Task 1
-        queue = {calibration_vals[0]}
-        for val2 in calibration_vals[1:]:
-            queue_new = {val1+val2 for val1 in queue}
-            queue_new.update({val1*val2 for val1 in queue})
-            queue = queue_new
-            
-        count += test_val*any([val == test_val for val in queue])
-                    
-        #Task 2
-        queue = {calibration_vals[0]}
-        for val2 in calibration_vals[1:]:
-            queue_new = {val1+val2 for val1 in queue}
-            queue_new.update({val1*val2 for val1 in queue})
-            queue_new.update({int(str(val1)+str(val2)) for val1 in queue})
-            queue = queue_new
-        count_with_concatenation += test_val*any([val == test_val for val in queue])
-            
-        
-    print("Total calibration result:", count)
-    print("Total calibration result with concatenation:", count_with_concatenation)
+    data = [(int(line.strip().split(":")[0]), tuple(int(val) for val in line.strip().split(":")[1].split())) for line in open(path+name+".txt")]
+    
+    pool = mp.Pool()
+    
+    #Task 1
+    start = time.time()
+    print("Total calibration result:", sum(pool.map_async(calibration_result, open(path+name+".txt")).get()))
+    end = time.time()
+    print("Runtime:", end-start)
+    
+    #Task 2
+    start = time.time()
+    print("Total calibration result with concatenation:", sum(pool.map_async(calibration_result_with_concatenation, open(path+name+".txt")).get()))
+    end = time.time()
+    print("Runtime:", end-start)
+    
+    pool.close()
         
     return
     
