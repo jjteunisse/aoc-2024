382af6034bbd2624cada7a2873020d4a0b309470
Margot Teunisse <margotteunisse@hotmail.com>
2024-12-08 09:28:51 +0100

    Solved day 8; again, could probably make this more compact by removing redundancy between tasks 1/2.

--- margot/day8.py
+++ margot/day8.py
@@ -1,5 +1,12 @@
 import sys
 import numpy as np
+from typing import Dict, Set, Tuple
+
+Frequency = str
+Position = Tuple[int, int]
+
+def all_antinodes(antinodes:Dict[Frequency, Set[Position]]) -> Set[Position]:
+    return set().union(*antinodes.values())
 
 def main():
     path = "inputs/day8/"
@@ -8,19 +15,34 @@ def main():
     with open(path+name+".txt") as file:
         data = np.array([list(line.strip()) for line in file])
 
-    antennas = {freq: {(i, j) for (i, j) in zip(*np.where(data == freq))} for freq in np.unique(data) if freq != '.'}
+    #Going to use Cartesian coordinates here, more intuitive when thinking about waves.
+    antennas = {freq: {(x, y) for (x, y) in zip(*np.where(data == freq))} for freq in np.unique(data) if freq != '.'}
     
     #Task 1 
     antinodes = {}
     for freq in antennas:
         #Generate antinodes.
-        antinodes[freq] = {(2*i1-i2, 2*j1-j2) for (i1, j1) in antennas[freq] for (i2, j2) in antennas[freq]-{(i1, j1)}}
+        antinodes[freq] = {(2*x1-x2, 2*y1-y2) for (x1, y1) in antennas[freq] for (x2, y2) in antennas[freq]-{(x1, y1)}}
         #Check that antinodes fall within input matrix. 
-        antinodes[freq] = {(i, j) for (i, j) in antinodes[freq] if (i >= 0 and j >= 0) and (i < data.shape[0] and j < data.shape[1])}
-    all_antinodes = set().union(*antinodes.values())
+        antinodes[freq] = {(x, y) for (x, y) in antinodes[freq] if (x >= 0 and y >= 0) and (x < data.shape[0] and y < data.shape[1])}
     
-    print("Number of unique antinode locations:", len(all_antinodes))
+    print("Number of unique antinode locations:", len(all_antinodes(antinodes)))
     
+    #(Task 2) Even as a physicist I find this frequency talk confusing...
+    #I think that antinodes are at multiples of the wave vector, i.e. the distance between antennas? 
+    wavevectors = {freq:{(x1, y1):{(x1-x2, y1-y2) for (x2, y2) in antennas[freq]-{(x1, y1)}} for (x1, y1) in antennas[freq]} for freq in antennas}
+    
+    antinodes = {freq:set() for freq in antennas}
+    for freq in antennas:
+        #I think the typical notation is k for the wave vector, so I will stick w/ this.
+        for pos in wavevectors[freq]:
+            for (kx, ky) in wavevectors[freq][pos]:
+                x, y = pos
+                while (x >= 0 and y >= 0) and (x < data.shape[0] and y < data.shape[1]):
+                    antinodes[freq].add((x, y))
+                    x += kx
+                    y += ky
+    print("Number of unique antinode locations for all integer multiples:", len(all_antinodes(antinodes)))
     
     return
     
