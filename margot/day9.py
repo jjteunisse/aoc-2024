843675fed76cc85cd364209d0c43172e1780fd07
Margot Teunisse <teunisse@Amolf.nl>
2024-12-09 16:07:01 +0100

    (Day 9) Switched around 'filled' and 'remaining' because this is easier to understand. Also added runtime checks.

--- margot/day9.py
+++ margot/day9.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import time
 
 def main():
     path = "inputs/day9/"
@@ -12,6 +13,8 @@ def main():
     free = data[1::2]
     
     #(Task 1) Probably not very efficient, but I'll start by doing this the way it was shown in the task.
+    start = time.time()
+    
     blocks = []
     for i in range(len(free)):
         blocks += [i]*files[i]
@@ -24,27 +27,34 @@ def main():
     blocks[free_blocks] = blocks[blocks != None][:-sum(free_blocks)-1:-1]
     blocks = blocks[:-sum(free_blocks)]
     
-    print("Checksum:", np.sum(blocks*np.arange(len(blocks))))
+    end = time.time()
     
-    #(Task 2) This seems easier to optimize since the files don't need to be split up.
+    print("Checksum:", np.sum(blocks*np.arange(len(blocks))))
+    print("Runtime:", end-start)
+    
+    #(Task 2) This seems easier to optimize since the files don't need to be split up - although it's significantly slower than task 1 in my execution.
+    start = time.time()
+    
     checksum = 0
     file_positions = np.cumsum([[0]+[files[i]+free[i] for i in range(len(free))]], dtype=np.int64)
     free_positions = np.cumsum([files[0]]+[files[i+1]+free[i] for i in range(len(free))], dtype=np.int64)
-    remaining = free.copy()
+    filled = [0]*len(free)
     for i in range(len(files)-1, -1, -1):
         moved = False
         for j in range(i):
-            if files[i] <= remaining[j]:
-                filled = free[j]-remaining[j]
-                starting_position = free_positions[j] + filled
+            if files[i] <= free[j]-filled[j]:
+                starting_position = free_positions[j] + filled[j]
                 checksum += np.sum(i*np.arange(starting_position, starting_position+files[i]), dtype=np.int64)
-                remaining[j] -= files[i]
+                filled[j] += files[i]
                 moved = True
                 break
         if not moved:
             checksum += np.sum(i*np.arange(file_positions[i], file_positions[i]+files[i]), dtype=np.int64)
+            
+    end = time.time()
         
     print("Checksum for whole file movement:", checksum)
+    print("Runtime:", end-start)
         
     return
 
