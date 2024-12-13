1561750c1362854b2966692f2afe659f9b309e69
Margot Teunisse <Teunisse@Physics.leidenuniv.nl>
2024-12-13 14:20:47 +0100

    Solved day 12, part 1; runtime issue was again simple fix where list was used i/o set.

--- margot/day12.py
+++ margot/day12.py
@@ -1,13 +1,10 @@
 import sys
 import numpy as np
-from typing import Tuple, Dict, Set, List
+from typing import Tuple, Dict, Set
+import time
         
 Position = Tuple[int, int]
 
-def neighbours(position:Position) -> List[Position]:
-    i, j = position
-    return [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
-        
 def map_regions(data:np.ndarray) -> Dict[int, Set[Position]]:
     plant_regions = {}
     mapping = {}
@@ -15,16 +12,20 @@ def map_regions(data:np.ndarray) -> Dict[int, Set[Position]]:
     for plant in np.unique(data):
         mask = (data == plant)
         plant_positions = set(zip(*np.where(mask)))
-        queue = [plant_positions.pop()]
+        queue = {plant_positions.pop()}
         while True:
+            #Explore region
             mapping[region] = set()
             while any(queue):
-                queue.extend([position for position in neighbours(queue[0]) if position in plant_positions-mapping[region]])
-                mapping[region].add(queue.pop(0))
+                (i, j) = queue.pop()
+                queue.update({position for position in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)] if position in plant_positions})
+                mapping[region].add((i, j))
+                plant_positions -= {(i, j)}
             plant_positions -= mapping[region]
             region += 1
+
             if any(plant_positions):
-                queue = [plant_positions.pop()]
+                queue = {plant_positions.pop()}
             else:
                 break
         
@@ -37,9 +38,12 @@ def main():
     with open(path+name+".txt") as file:
         data = np.array([list(line.strip()) for line in file])
         
-    region_mapping = map_regions(data)
     
     #Task 1
+    start = time.time()
+    
+    region_mapping = map_regions(data)
+    
     areas = {region:len(region_mapping[region]) for region in region_mapping}
     perimeters = {region: 0 for region in region_mapping}
     for region in perimeters:
@@ -49,9 +53,12 @@ def main():
             perimeters[region] += not (i, j-1) in region_mapping[region]
             perimeters[region] += not (i, j+1) in region_mapping[region]
     
+    fencing_price = sum([perimeters[region]*areas[region] for region in region_mapping])
     
-    fencing_price = sum({perimeters[region]*areas[region] for region in region_mapping})
+    end = time.time()
+    
     print("Total price of fencing:", fencing_price)
+    print("Runtime:", end-start)
     
     return
     
