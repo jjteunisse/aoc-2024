c1dfc7a40eba6524f1473f8a895160690b859b36
jjteunisse <joristeunisse@gmail.com>
2024-12-21 13:18:43 +0100

    [d9] p2 done, maar werk in uitvoering

--- joris/src/day_9.py
+++ joris/src/day_9.py
@@ -20,7 +20,34 @@ def part_1(data: str) -> int:
 	return sum([i * int(x) for i, x in enumerate(result) if x != '.'])
 
 def part_2(data: str) -> int:
-	pass
+	blocks = []
+	for i, digit in enumerate(data):
+		if i % 2 == 0:
+			blocks.append([str(i // 2), int(digit)])
+		else:
+			blocks.append(['.', int(digit)])
+
+	i = len(blocks) - 1
+	while i > 0:
+		if blocks[i][0] != '.':
+			j = 0
+			while j < i:
+				if blocks[j][0] == '.':
+					if blocks[j][1] == blocks[i][1]:
+						blocks[i], blocks[j] = blocks[j], blocks[i]
+						break
+					elif blocks[j][1] > blocks[i][1]:
+						blocks[j][1] -= blocks[i][1]
+						blocks.insert(j, blocks[i][:])
+						blocks[i+1][0] = '.'
+						break
+				j += 1
+		i -= 1
+
+	result = []
+	for b in blocks: result += [b[0]] * b[1]
+
+	return sum([i * int(x) for i, x in enumerate(result) if x != '.'])
 
 def run(data: str, parts: list[str]):
 	if 'p1' in parts:
