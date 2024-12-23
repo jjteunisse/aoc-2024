637a13826a91de32c97701edae6e02168357fb6c
jjteunisse <joristeunisse@gmail.com>
2024-12-23 13:07:58 +0100

    coole ranking ingebouwd

--- joris/run.py
+++ joris/run.py
@@ -1,4 +1,4 @@
-# (venv) > py run.py <day> [-cp] [-lp] [-p1] [-p2] [-t]
+# (venv) > py run.py [day] [-cp] [-lp] [-p1] [-p2] [-t]
 
 from cProfile import Profile
 from line_profiler import LineProfiler
@@ -7,35 +7,56 @@ from time import time
 from types import FunctionType
 
 if __name__ == '__main__':
-	day = f'day_{argv[1]}'
-	env = 'tst' if '-t' in argv else 'prd'
+	d_start, d_end = 1, 10
+	if len(argv) > 1 and all([x.isdigit() for x in argv[1]]):
+		d_start, d_end = int(argv[1]), int(argv[1]) + 1
 
-	parts = ['p1', 'p2']
-	if   '-p1' in argv: parts.remove('p2')
-	elif '-p2' in argv: parts.remove('p1')
+	ranking = []
+	for d in range(d_start, d_end):
+		print(f'\nRunning day {d}...')
 
-	path.append('src')
-	module = __import__(day)
-	data = open(f'data/{env}/{day}.txt').read()
-	if '-cp' in argv:
-		profiler = Profile()
-		profiler.enable()
-		module.run(data, parts)
-		profiler.disable()
-		profiler.print_stats(sort='tottime')
-	elif '-lp' in argv:
-		profiler = LineProfiler()
-		for func in dir(module):
-			if (
-				isinstance(getattr(module, func), FunctionType) and
-				not func.startswith('_')
-			):
-				profiler.add_function(getattr(module, func))
-		profiler.enable()
-		module.run(data, parts)
-		profiler.disable()
-		profiler.print_stats()
-	else:
-		start = time()
-		module.run(data, parts)
-		print(f'Total runtime: {time() - start:.3f} seconds.')
+		day = f'day_{d}'
+		env = 'tst' if '-t' in argv else 'prd'
+
+		parts = ['p1', 'p2']
+		if   '-p1' in argv: parts.remove('p2')
+		elif '-p2' in argv: parts.remove('p1')
+
+		path.append('src')
+		module = __import__(day)
+		data = open(f'data/{env}/{day}.txt').read()
+		if '-cp' in argv:
+			profiler = Profile()
+			profiler.enable()
+			module.run(data, parts)
+			profiler.disable()
+			print()
+			profiler.print_stats(sort='tottime')
+		elif '-lp' in argv:
+			profiler = LineProfiler()
+			for func in dir(module):
+				if (
+					isinstance(getattr(module, func), FunctionType) and
+					not func.startswith('_')
+				):
+					profiler.add_function(getattr(module, func))
+			profiler.enable()
+			module.run(data, parts)
+			profiler.disable()
+			print()
+			profiler.print_stats()
+		else:
+			start = time()
+			module.run(data, parts)
+			runtime = time() - start
+			ranking.append((d, runtime))
+			print(f'Runtime: {runtime:.3f} seconds.')
+
+	if d_start != d_end - 1 and [flag not in argv for flag in ['-cp', '-lp']]:
+		ranking.sort(key=lambda tup: tup[1])
+		total_runtime = sum(tup[1] for tup in ranking)
+		print(f'\nRuntime ranking:')
+		for i, tup in enumerate(ranking):
+			print(f'Rank {i + 1:>02} - Day {tup[0]:>02} - {tup[1]:.3f} seconds.')
+		print(' ' * 19 + '-' * 14)
+		print(f'{' ' * 12} Total {total_runtime:.3f} seconds.')
