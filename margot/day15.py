4c1d42dfc9b954331d752d1f4a1125598d948b52
Margot Teunisse <margotteunisse@hotmail.com>
2024-12-23 12:56:58 +0100

    (Day 15) Nothing apparently wrong now, but resulting configuration for larger test is still drastically different than in the exercise - why?

--- margot/day15.py
+++ margot/day15.py
@@ -31,8 +31,9 @@ def task1(instructions:List[Direction], data:np.ndarray) -> int:
     return sum([100*i + j for (i, j) in boxes])
     
 def task2(instructions:List[Direction], data:np.ndarray) -> int:
-    walls = set((i, 2*j) for (i, j) in zip(*np.where(data == "#")))
-    boxes = set((i, 2*j) for (i, j) in zip(*np.where(data == "O")))
+    walls = {(i, 2*j) for (i, j) in zip(*np.where(data == "#"))}
+    walls.update({(i, 2*j+1) for (i, j) in zip(*np.where(data == "#"))})
+    boxes = {(i, 2*j) for (i, j) in zip(*np.where(data == "O"))}
     i, j = next(zip(*np.where(data == "@")))
     robot = (i, 2*j)
     
@@ -41,20 +42,19 @@ def task2(instructions:List[Direction], data:np.ndarray) -> int:
         
     for position in walls:
         image[position] = '#'
-        image[position[0], position[1]+1] = '#'
     
     for direction in instructions:
         image[robot] = mapping[direction]
         robot = (robot[0]+direction[0], robot[1]+direction[1])
         queue = {(robot[0], j) for j in range(robot[1]-1, robot[1]+1) if (robot[0], j) in boxes}
         pushed = set()
-        allowed = [(i, j) in walls for j in range(robot[0]-1, robot[1]+2)]
+        allowed = not any([(robot[0], j) in walls for j in range(robot[1], robot[1]+2)])
         while allowed and any(queue):
             pushed.update(queue)
             queue_new = set()
             for position in queue:
                 i = position[0]+direction[0]
-                if any([(i, j) in walls for j in range(position[1]+direction[1]-1, position[1]+direction[1]+2)]):
+                if any([(i, j) in walls for j in range(position[1]+direction[1], position[1]+direction[1]+2)]):
                     allowed = False
                     break
                 queue_new.update({(i, j) for j in range(position[1]+direction[1]-1, position[1]+direction[1]+2) if (i, j) in boxes - pushed})
@@ -67,14 +67,13 @@ def task2(instructions:List[Direction], data:np.ndarray) -> int:
                 boxes -= pushed
                 boxes.update({(position[0]+direction[0], position[1]+direction[1]) for position in pushed})
                 
-        if any(pushed) and allowed:
-            print(pushed)
-            image[robot] = '@'
-            for position in boxes:
-                image[position] = '['
-                image[position[0], position[1]+1] = ']'
-            show(image)
-            image[image != '#'] = '.'
+        image[robot] = '@'
+        for position in boxes:
+            image[position] = '['
+            image[position[0], position[1]+1] = ']'
+        show(image)
+        image[image != '#'] = '.'
+        time.sleep(1)
         
     
     return sum([100*i + j for (i, j) in boxes])
