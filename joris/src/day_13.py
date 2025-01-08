18f5d52d91147c3beee1668c5db7ede0619a1a0f
jjteunisse <joristeunisse@gmail.com>
2025-01-08 16:08:56 +0100

    [d13] refactor

--- joris/src/day_13.py
+++ joris/src/day_13.py
@@ -4,32 +4,13 @@ import sympy as sp
 def solve(data: str, *, part: int) -> int:
 	total_cost = 0
 	for block in data.split('\n\n'):
-		ax, ay, bx, by = [int(x) for x in re.findall(r'\+(\d+)', block)]
-		xt, yt = [int(x) for x in re.findall(r'=(\d+)', block)]
+		ax, ay, bx, by, x, y = [int(x) for x in re.findall(r'\d+', block)]
+		if part == 2: x, y = x + int(1E13), y + int(1E13)
 
-		if part == 1:
-			i = 0
-			while xt >= 0 and yt >= 0:
-				if xt % ax == 0 and yt % ay == 0 and xt // ax == yt // ay:
-					total_cost += (xt // ax) * 3 + i
-					break
-				xt -= bx
-				yt -= by
-				i += 1
-
-		elif part == 2:
-			xt += int(1E13)
-			yt += int(1E13)
-			a, b = sp.symbols('a, b')
-			result = sp.solve(
-				[a * ax + b * bx - xt, a * ay + b * by - yt],
-				a, b
-			)
-			if (
-				isinstance(result[a], sp.core.numbers.Integer) and
-				isinstance(result[b], sp.core.numbers.Integer)
-			):
-				total_cost += 3 * result[a] + result[b]
+		a, b = sp.symbols('a, b')
+		result = sp.solve([a * ax + b * bx - x, a * ay + b * by - y], a, b)
+		if type(result[a]) == type(result[b]) == sp.core.numbers.Integer:
+			total_cost += 3 * result[a] + result[b]
 
 	return total_cost
 
