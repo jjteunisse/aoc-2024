c2c49608efad8eaf389918cf7a2f322e4418165d
jjteunisse <joristeunisse@gmail.com>
2025-03-01 15:42:04 +0100

    [d15] p2 :)

--- joris/src/day_15.py
+++ joris/src/day_15.py
@@ -14,12 +14,25 @@ def print_grid(grid: Grid):
 			color = None
 			match char:
 				case '@': color = cr.Fore.RED
-				case '#': color = cr.Fore.BLUE
-				case 'O': color = cr.Fore.GREEN
 				case '.': color = cr.Fore.WHITE
+				case '#': color = cr.Fore.BLUE
+				case 'O' | '[' | ']': color = cr.Fore.GREEN
 			print(color + char, end='')
 		print()
 	print()
+
+def resize_grid(grid: Grid) -> Grid:
+	new_grid = []
+	for line in grid:
+		new_line = []
+		for char in line:
+			match char:
+				case '#': new_line += ['#', '#']
+				case 'O': new_line += ['[', ']']
+				case '@': new_line += ['@', '.']
+				case '.': new_line += ['.', '.']
+		new_grid.append(new_line)
+	return new_grid
 
 def part_1(data: str) -> int:
 	cr.init(autoreset=True)
@@ -64,7 +77,82 @@ def part_1(data: str) -> int:
 	])
 
 def part_2(data: str) -> int:
-	pass
+	cr.init(autoreset=True)
+
+	grid, moves = data.split('\n\n')
+	grid = [list(line) for line in grid.split('\n')]
+	grid = resize_grid(grid)
+	moves = moves.replace('\n', '')
+	ry, rx = find_robot(grid)
+
+	for i, move in enumerate(moves):
+		# == DEBUG ==
+		# print(f'Iteration {i} (next: {move})')
+		# print_grid(grid)
+
+		dy, dx = 0, 0
+		match move:
+			case '^': dy = -1
+			case '>': dx = 1
+			case 'v': dy = 1
+			case '<': dx = -1
+
+		if grid[ry + dy][rx + dx] != '#':
+			if grid[ry + dy][rx + dx] in '[]':
+				if move in '><':
+					x_off = dx
+					while grid[ry][rx + x_off] in '[]':
+						x_off += dx
+					if grid[ry][rx + x_off] == '.':
+						block = '[]' if move == '>' else ']['
+						for x in range(dx, x_off + dx, dx):
+							grid[ry][rx + x] = block[x % 2]
+					else:
+						continue
+
+				elif move in '^v':
+					todo = [(ry, rx)]
+					targets = []
+					while todo != []:
+						ty, tx = todo[0]
+						if grid[ty][tx] != '@':
+							targets.append((ty, tx))
+
+						ty += dy
+						if grid[ty][tx] == '#':
+							break
+						elif grid[ty][tx] == '[':
+							if (ty, tx) not in todo:
+								todo.append((ty, tx))
+							if (ty, tx + 1) not in todo:
+								todo.append((ty, tx + 1))
+						elif grid[ty][tx] == ']':
+							if (ty, tx - 1) not in todo:
+								todo.append((ty, tx - 1))
+							if (ty, tx) not in todo:
+								todo.append((ty, tx))
+
+						del todo[0]
+
+					if todo == []:
+						for t in reversed(targets):
+							ty, tx = t
+							grid[ty + dy][tx] = grid[ty][tx]
+							grid[ty][tx] = '.'
+					else:
+						continue
+
+			grid[ry][rx] = '.'
+			ry += dy
+			rx += dx
+			grid[ry][rx] = '@'
+
+	return sum([
+		y * 100 + x
+		for y, line in enumerate(grid)
+		for x, char in enumerate(line)
+		if char == '['
+	])
 
 def run(data: str, parts: list[str]):
 	if 'p1' in parts:
