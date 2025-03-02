70895cbeb9953181a5ee6fb0711b56938f2db3b1
jjteunisse <joristeunisse@gmail.com>
2025-03-02 08:57:25 +0100

    [d15] p1 & p2 samengevoegd

--- joris/src/day_15.py
+++ joris/src/day_15.py
@@ -1,12 +1,39 @@
 import colorama as cr
 
 type Grid = list[list[str]]
+type Point = tuple[int, int]
 
-def find_robot(grid: Grid) -> tuple[int, int]:
+def find_robot(grid: Grid) -> Point:
 	for y, line in enumerate(grid):
 		for x, char in enumerate(line):
 			if char == '@':
 				return y, x
+
+def get_targets(grid: Grid, ry: int, rx: int, dy: int) -> list[Point]:
+	todo = [(ry, rx)]
+	targets = []
+	while todo != []:
+		ty, tx = todo[0]
+		if grid[ty][tx] != '@':
+			targets.append((ty, tx))
+
+		ty += dy
+		if grid[ty][tx] == '#':
+			return []
+		elif grid[ty][tx] == '[':
+			if (ty, tx) not in todo:
+				todo.append((ty, tx))
+			if (ty, tx + 1) not in todo:
+				todo.append((ty, tx + 1))
+		elif grid[ty][tx] == ']':
+			if (ty, tx - 1) not in todo:
+				todo.append((ty, tx - 1))
+			if (ty, tx) not in todo:
+				todo.append((ty, tx))
+
+		del todo[0]
+
+	return targets
 
 def print_grid(grid: Grid):
 	for line in grid:
@@ -32,16 +59,18 @@ def resize_grid(grid: Grid) -> Grid:
 				case '@': new_line += ['@', '.']
 				case '.': new_line += ['.', '.']
 		new_grid.append(new_line)
+
 	return new_grid
 
-def part_1(data: str) -> int:
+def solve(data: str, *, part: int) -> int:
 	cr.init(autoreset=True)
 
 	grid, moves = data.split('\n\n')
 	grid = [list(line) for line in grid.split('\n')]
+	if part == 2: grid = resize_grid(grid)
 	moves = moves.replace('\n', '')
-	ry, rx = find_robot(grid)
 
+	ry, rx = find_robot(grid)
 	for i, move in enumerate(moves):
 		# == DEBUG ==
 		# print(f'Iteration {i} (next: {move})')
@@ -64,77 +93,22 @@ def part_1(data: str) -> int:
 					grid[ry + y_off][rx + x_off] = 'O'
 				else:
 					continue
-			grid[ry][rx] = '.'
-			ry += dy
-			rx += dx
-			grid[ry][rx] = '@'
 
-	return sum([
-		y * 100 + x
-		for y, line in enumerate(grid)
-		for x, char in enumerate(line)
-		if char == 'O'
-	])
-
-def part_2(data: str) -> int:
-	cr.init(autoreset=True)
-
-	grid, moves = data.split('\n\n')
-	grid = [list(line) for line in grid.split('\n')]
-	grid = resize_grid(grid)
-	moves = moves.replace('\n', '')
-	ry, rx = find_robot(grid)
-
-	for i, move in enumerate(moves):
-		# == DEBUG ==
-		# print(f'Iteration {i} (next: {move})')
-		# print_grid(grid)
-
-		dy, dx = 0, 0
-		match move:
-			case '^': dy = -1
-			case '>': dx = 1
-			case 'v': dy = 1
-			case '<': dx = -1
-
-		if grid[ry + dy][rx + dx] != '#':
-			if grid[ry + dy][rx + dx] in '[]':
+			elif grid[ry + dy][rx + dx] in '[]':
 				if move in '><':
 					x_off = dx
 					while grid[ry][rx + x_off] in '[]':
 						x_off += dx
 					if grid[ry][rx + x_off] == '.':
 						block = '[]' if move == '>' else ']['
-						for x in range(dx, x_off + dx, dx):
+						for x in range(dx, dx + x_off, dx):
 							grid[ry][rx + x] = block[x % 2]
 					else:
 						continue
 
 				elif move in '^v':
-					todo = [(ry, rx)]
-					targets = []
-					while todo != []:
-						ty, tx = todo[0]
-						if grid[ty][tx] != '@':
-							targets.append((ty, tx))
-
-						ty += dy
-						if grid[ty][tx] == '#':
-							break
-						elif grid[ty][tx] == '[':
-							if (ty, tx) not in todo:
-								todo.append((ty, tx))
-							if (ty, tx + 1) not in todo:
-								todo.append((ty, tx + 1))
-						elif grid[ty][tx] == ']':
-							if (ty, tx - 1) not in todo:
-								todo.append((ty, tx - 1))
-							if (ty, tx) not in todo:
-								todo.append((ty, tx))
-
-						del todo[0]
-
-					if todo == []:
+					targets = get_targets(grid, ry, rx, dy)
+					if targets != []:
 						for t in reversed(targets):
 							ty, tx = t
 							grid[ty + dy][tx] = grid[ty][tx]
@@ -151,11 +125,11 @@ def part_2(data: str) -> int:
 		y * 100 + x
 		for y, line in enumerate(grid)
 		for x, char in enumerate(line)
-		if char == '['
+		if char in 'O['
 	])
 
 def run(data: str, parts: list[str]):
 	if 'p1' in parts:
-		print(f'The answer to part 1 is: {part_1(data)}.')
+		print(f'The answer to part 1 is: {solve(data, part=1)}.')
 	if 'p2' in parts:
-		print(f'The answer to part 2 is: {part_2(data)}.')
+		print(f'The answer to part 2 is: {solve(data, part=2)}.')
